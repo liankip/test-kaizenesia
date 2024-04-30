462c318cc7251b6222411da6815e3f209a79ff2c
liankip <simdigliandi@gmail.com>
2024-04-30 19:26:54 +0700

    first commit

--- soal2.js
+++ soal2.js
@@ -15,14 +15,14 @@ function progressive(val) {
             break
         case val >= 10000 && val <= 100000:
             result += 5000 * 0.05 // dikenakan 0.05 = 5%
-            result += 5000 * 0.1 // dikenakan 0.1 = 10%
-            result += (val - 10000) * 0.15 // jika case range 5001 - 10000 maka value dikalikan 0.15 = 15%
+            result += 10000 * 0.1 // dikenakan 0.1 = 10%
+            result += (val - 10000) * 0.15 // jika case range 10000 - 100000 maka value dikalikan 0.15 = 15%
             break
         case val >= 100000:
             result += 5000 * 0.05; // dikenakan 0.05 = 5%
-            result += 5000 * 0.1; // dikenakan 0.1 = 10%
-            result += 82000 * 0.15; // dikenakan 0.15 = 15%
-            result += (val - 100000) * 0.2; // jika case range 5001 - 10000 maka value dikalikan 0.2 = 20%
+            result += 10000 * 0.1; // dikenakan 0.1 = 10%
+            result += 100000 * 0.15; // dikenakan 0.15 = 15%
+            result += (val - 100000) * 0.2; // jika case range 100000 lebih maka value dikalikan 0.2 = 20%
             break
     }
 
